1af7286486d9048a7146bc1aad4898f37b203e35
Daniel Oliveira <n12018520@qut.edu.au>
2026-04-22 14:44:22 +1000

    update to database

--- demo/src/main/java/com/cab302thursdaytbd/Database.java
+++ demo/src/main/java/com/cab302thursdaytbd/Database.java
@@ -9,6 +9,13 @@ public class Database {
             "jdbc:sqlite:C:\\Users\\dd19o\\OneDrive - Queensland University of Technology\\Uni\\Year 3, Sem 1\\CAB302\\Project\\CAB302_DesktopPet_ThursdayTBD\\demo\\src\\main\\resources\\com\\cab302thursdaytbd\\database\\petapp.sqlite";
 
     public static Connection connect() throws SQLException {
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("SQLite driver not found", e);
+        }
+
+
         return DriverManager.getConnection(DB_URL);
     }
 }
