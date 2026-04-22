baa280c2723ce47faf7bdad50f8f527963b9e4d3
Albert <n@>
2026-04-22 19:27:17 +1000

    Added sqlite to project library and fixed pathing

--- demo/src/main/java/com/cab302thursdaytbd/Database.java
+++ demo/src/main/java/com/cab302thursdaytbd/Database.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class Database {
     private static final String DB_URL =
-            "jdbc:sqlite:C:\\Users\\dd19o\\OneDrive - Queensland University of Technology\\Uni\\Year 3, Sem 1\\CAB302\\Project\\CAB302_DesktopPet_ThursdayTBD\\demo\\src\\main\\resources\\com\\cab302thursdaytbd\\database\\petapp.sqlite";
+            "jdbc:sqlite:" + System.getProperty("user.dir") + "\\demo\\src\\main\\resources\\com\\cab302thursdaytbd\\database\\petapp.sqlite";
 
     public static Connection connect() throws SQLException {
         try {
--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.cab302thursdaytbd {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
     opens com.cab302thursdaytbd to javafx.fxml;
     exports com.cab302thursdaytbd;
