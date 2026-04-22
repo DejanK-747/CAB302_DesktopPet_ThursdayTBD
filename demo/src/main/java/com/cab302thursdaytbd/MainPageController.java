0ed679a5cc04287740413311dd7638824fd31266
Albert <n@>
2026-04-22 16:20:40 +1000

    Added pop-up animation

--- demo/src/main/java/com/cab302thursdaytbd/MainPageController.java
+++ demo/src/main/java/com/cab302thursdaytbd/MainPageController.java
@@ -1,8 +1,17 @@
 package com.cab302thursdaytbd;
 
+import com.cab302thursdaytbd.Functions.DraggableMaker;
+import javafx.animation.Interpolator;
+import javafx.animation.ScaleTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.util.Duration;
 
 public class MainPageController {
 
@@ -10,13 +19,34 @@ public class MainPageController {
     private Button interactButton1;
 
     @FXML
-    private HBox popUp1;
+    private ImageView image1;
+
+    @FXML
+    private ImageView petView;
+
+    @FXML
+    private AnchorPane popUp1;
 
 
     @FXML
     public void showPopUp1() {
-        popUp1.setVisible(!popUp1.isVisible());
 
+        if (popUp1.getScaleX() == 0) {
+            ScaleTransition transition = new ScaleTransition(Duration.seconds(0.25), popUp1);
+            transition.setToX(1);
+            transition.setToY(1);
+            transition.setInterpolator(Interpolator.LINEAR);
+
+            transition.play();
+
+        } else if (popUp1.getScaleX() == 1){
+            ScaleTransition transition = new ScaleTransition(Duration.seconds(0.25), popUp1);
+            transition.setToX(0);
+            transition.setToY(0);
+            transition.setInterpolator(Interpolator.LINEAR);
+
+            transition.play();
+        }
     }
 
 }
