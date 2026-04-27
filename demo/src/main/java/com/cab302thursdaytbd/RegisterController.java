2bfe66f956916a8ed589970e053effc27229916c
Dejan Kanazir <kanazirdenny@gmail.com>
2026-04-27 16:23:43 +1000

    Updated register to include 'confirm password' field

--- demo/src/main/java/com/cab302thursdaytbd/RegisterController.java
+++ demo/src/main/java/com/cab302thursdaytbd/RegisterController.java
@@ -12,15 +12,23 @@ public class RegisterController {
     private PasswordField passwordField;
 
     @FXML
+    private PasswordField confirmPasswordField;
+
+    @FXML
     private Label statusLabel;
 
     @FXML
     private void handleRegister() {
         String username = usernameField.getText().trim();
         String password = passwordField.getText().trim();
+        String confirmPassword = confirmPasswordField.getText().trim();
 
-        if (username.isEmpty() || password.isEmpty()) {
+        if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
             statusLabel.setText("Fields cannot be empty");
+            return;
+        }
+        if (!password.equals(confirmPassword)) {
+            statusLabel.setText("Passwords do not match");
             return;
         }
 
