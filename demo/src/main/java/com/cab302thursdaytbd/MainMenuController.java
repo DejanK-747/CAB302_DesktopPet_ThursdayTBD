5a85a844ae6c6782e8c0d21e4a17df1ace7ab452
Callum N <callumnar@outlook.com>
2026-04-30 15:16:17 +1000

    added Main Menu and code for functionality and the fxml for the about the app page which still needs to be written as well as the kill button UI design.

---
+++ demo/src/main/java/com/cab302thursdaytbd/MainMenuController.java
@@ -0,0 +1,24 @@
+package com.cab302thursdaytbd;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class MainMenuController {
+
+    @FXML
+    private void switchToStatistics() throws IOException {
+        App.setRoot("stats");
+    }
+
+    @FXML
+    private void switchToAbout() throws IOException {
+        App.setRoot("about_app");
+    }
+
+    @FXML
+    private void switchToLogin() throws IOException {
+        App.setRoot("login");
+    }
+
+
+}
