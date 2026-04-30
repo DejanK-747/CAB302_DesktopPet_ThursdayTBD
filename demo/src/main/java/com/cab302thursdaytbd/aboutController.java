96a38217ca7a505cefdbc4e64475b0e6938cfef0
Callum N <callumnar@outlook.com>
2026-04-30 15:38:23 +1000

    Continued work on about the app page. However, issues have begun with the scene builder.

--- demo/src/main/java/com/cab302thursdaytbd/aboutController.java
+++ demo/src/main/java/com/cab302thursdaytbd/aboutController.java
@@ -1,4 +1,11 @@
 package com.cab302thursdaytbd;
 
+import javafx.fxml.FXML;
+import java.io.IOException;
+
 public class aboutController {
+    @FXML
+    private void switchToMainMenu() throws IOException{
+        App.setRoot("main_menu");
+    }
 }
