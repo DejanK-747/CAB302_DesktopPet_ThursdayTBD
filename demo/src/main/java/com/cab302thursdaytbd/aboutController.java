37fe3af7b198fdc6b46b6bd5d44827184c74195f
Callum N <callumnar@outlook.com>
2026-05-14 12:57:33 +1000

    fixed issue with the aboutController which caused an issue with mouse exiting

--- demo/src/main/java/com/cab302thursdaytbd/aboutController.java
+++ demo/src/main/java/com/cab302thursdaytbd/aboutController.java
@@ -1,11 +1,12 @@
 package com.cab302thursdaytbd;
 
-import javafx.fxml.FXML;
 import java.io.IOException;
+import javafx.fxml.FXML;
 
 public class aboutController {
+
     @FXML
-    private void switchToMainMenu() throws IOException{
+    private void switchToMainMenu() throws IOException {
         App.setRoot("main_menu");
     }
 }
