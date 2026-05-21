b4d965ebb4aa2b7d94610c2fbc58f3dafe680d5f
Callum N <callumnar@outlook.com>
2026-05-21 16:36:12 +1000

    Updated about page and controller for full functionality and text

--- demo/src/main/java/com/cab302thursdaytbd/AboutController.java
+++ demo/src/main/java/com/cab302thursdaytbd/AboutController.java
@@ -1,4 +1,19 @@
 package com.cab302thursdaytbd;
 
+import java.io.IOException;
+
+import com.cab302thursdaytbd.Model.Session;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
 public class AboutController {
+    @FXML
+    private void switchToMainMenu() {
+        try {
+            App.setRoot("main_menu");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
